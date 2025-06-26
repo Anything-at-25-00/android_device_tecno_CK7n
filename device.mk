8ca877a5da7d80a8d56db99521262cedc56778ca
baka-hokage <ayushbehera007@gmail.com>
2025-06-26 14:20:55 +0900

    CK7n: Include JamesDSP

--- device.mk
+++ device.mk
@@ -241,6 +241,9 @@ PRODUCT_PACKAGES += \
     init.sensor_2_0.rc \
     ueventd.mt6789.rc
 
+# JamesDSP
+$(call inherit-product-if-exists, vendor/JamesDSP/config.mk)
+
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.lights-service.millennium
