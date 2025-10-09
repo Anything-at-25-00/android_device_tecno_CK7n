309b0d8c0c44ade15eab9ecc9887c321da040013
Shirayuki39 <lorddemecrius83@proton.me>
2025-10-10 06:13:59 +0900

    CK7n: Initial Skeleton Tree
    Signed-off-by: Shirayuki39 <lorddemecrius83@proton.me>

---
+++ device.mk
@@ -0,0 +1,14 @@
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Shipping API level
+PRODUCT_SHIPPING_API_LEVEL := 33
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
+# Inherit the proprietary files
+$(call inherit-product, vendor/tecno/CK7n/CK7n-vendor.mk)
