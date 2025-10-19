60b892f8709dad5bf080c0f834fd1d24b914777e
Shirayuki39 <lorddemecrius83@proton.me>
2025-10-20 04:03:14 +0900

    CK7n: vendorsetup: Add fwb FOD support patch
    Signed-off-by: Shirayuki39 <lorddemecrius83@proton.me>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,3 +4,7 @@ echo "- Applying Aperture Mediatek HFPS Mode Patch"
 cd packages/apps/Aperture
 curl https://github.com/Nothing-2A/android_packages_apps_Aperture/commit/9509277efc852ad8bdcce204e0d9cfe104b6d190.patch | git am
 cd ../../../
+echo "- Applying fwb FOD patch"
+cd frameworks/base
+curl https://raw.githubusercontent.com/Anything-at-25-00/patches/refs/heads/sixteen/frameworks/base/0001-SystemUI-biometrics-Add-HBM-Trigger-for-Transsion-UD.patch | git am
+cd ../..
