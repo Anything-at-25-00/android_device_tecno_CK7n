3ab2c022024845a02883feeff6d66d41e078a132
Shirayuki39 <lorddemecrius83@proton.me>
2025-10-20 00:10:19 +0900

    CK7n: Build AIDL Millennium Lights HAL
    * Remember, f transsion
    
    Signed-off-by: Shirayuki39 <lorddemecrius83@proton.me>

--- device.mk
+++ device.mk
@@ -163,6 +163,10 @@ PRODUCT_PACKAGES += \
     FrameworkResOverlayPyroath \
     SystemUIResOverlayPyroath
 
+# Lights
+PRODUCT_PACKAGES += \
+    android.hardware.lights-service.millennium
+
 # Keymaster
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.keystore.app_attest_key.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.keystore.app_attest_key.xml
