a59565320c016901d1d7f21d7ff44b0dd1e49205
Shirayuki39 <lorddemecrius83@proton.me>
2025-10-21 12:16:42 +0900

    CK7n: vendorsetup: Refactor vendorsetup.sh
    * So if a patch fails, it won't leave source trees in a broken state
    
    Signed-off-by: Shirayuki39 <lorddemecrius83@proton.me>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,9 +2,14 @@
 
 echo "- Applying Aperture Mediatek HFPS Mode Patch"
 cd packages/apps/Aperture
-curl https://github.com/Nothing-2A/android_packages_apps_Aperture/commit/9509277efc852ad8bdcce204e0d9cfe104b6d190.patch | git am
+curl https://github.com/Nothing-2A/android_packages_apps_Aperture/commit/9509277efc852ad8bdcce204e0d9cfe104b6d190.patch | git am || {
+  RET=$?
+  git am --abort >/dev/null 2>&1
+}
 cd ../../../
-echo "- Applying fwb FOD patch"
-cd frameworks/base
-curl https://raw.githubusercontent.com/Anything-at-25-00/patches/refs/heads/sixteen/frameworks/base/0001-SystemUI-biometrics-Add-HBM-Trigger-for-Transsion-UD.patch | git am
-cd ../..
+
+if [ $RET -ne 0 ]; then
+  echo "ERROR: Patch is not applied! Maybe it's already patched, or you'll have to adapt it to this specific rom source?"
+else
+  echo "OK: All patched"
+fi
